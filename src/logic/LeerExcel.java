f167b0893194eadd16cd5dff43d35534aacc0df9
Sebastian Becerra <juan.becerra02@uptc.edu.co>
2018-10-02 15:32:38 -0500

    Prueba primer commit

--- src/logic/LeerExcel.java
+++ src/logic/LeerExcel.java
@@ -13,6 +13,7 @@ public class LeerExcel {
     	
     	libro.readExcelFile("E:\\seta_projects\\constructora\\documentos_constructora\\", "FORMATO MEMORIAS DE CANTIDADES-ELECTRICAS.xlsx");
     	//prueba del primer commit 
+    	//en serio es una prueba
     	libro.readSheet("10.1");
     	for (int i = 0; i < libro.getDataMap().size(); i++) {
     		for (int j = 0; j < libro.getDataMap().get(i).length; j++) {
