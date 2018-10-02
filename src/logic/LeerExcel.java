7cd770d18e38ee6f2ae0ac740ff22e1cc24be3e6
Sebastian Becerra <juan.becerra02@uptc.edu.co>
2018-10-02 15:31:23 -0500

    El primer commit del repositorio

--- src/logic/LeerExcel.java
+++ src/logic/LeerExcel.java
@@ -12,7 +12,7 @@ public class LeerExcel {
     	ExcelBook libro = new ExcelBook();
     	
     	libro.readExcelFile("E:\\seta_projects\\constructora\\documentos_constructora\\", "FORMATO MEMORIAS DE CANTIDADES-ELECTRICAS.xlsx");
-    	
+    	//prueba del primer commit 
     	libro.readSheet("10.1");
     	for (int i = 0; i < libro.getDataMap().size(); i++) {
     		for (int j = 0; j < libro.getDataMap().get(i).length; j++) {
