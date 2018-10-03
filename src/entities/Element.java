6c9a624872606b1159ccb808a1fe5c6b26cb4509
Sebastian Becerra <juan.becerra02@uptc.edu.co>
2018-10-03 10:45:47 -0500

    Creacion de las entidades Element y TypeElement

--- src/entities/Element.java
+++ src/entities/Element.java
@@ -1,5 +1,50 @@
 package entities;
 
 public class Element {
-
+	
+	private int idAPU;
+	private int idElement;
+	private String name;
+	private String unity;
+	private int price;
+	private int quantity;
+	public int getIdAPU() {
+		return idAPU;
+	}
+	public void setIdAPU(int idAPU) {
+		this.idAPU = idAPU;
+	}
+	public int getIdElement() {
+		return idElement;
+	}
+	public void setIdElement(int idElement) {
+		this.idElement = idElement;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getUnity() {
+		return unity;
+	}
+	public void setUnity(String unity) {
+		this.unity = unity;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	
+	
+	
 }
---
+++ src/entities/TypeElement.java
@@ -0,0 +1,27 @@
+package entities;
+
+public class TypeElement {
+
+	private int idTypeelement;
+	private String name;
+	
+	
+	
+	
+	public int getIdTypeelement() {
+		return idTypeelement;
+	}
+	public void setIdTypeelement(int idTypeelement) {
+		this.idTypeelement = idTypeelement;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+	
+}
