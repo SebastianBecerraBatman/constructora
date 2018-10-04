e54821008838a75c3d3a8509bfd59590fbabe74a
Sebastian Becerra <juan.becerra02@uptc.edu.co>
2018-10-04 17:59:27 -0500

    Escribe excel concatenando el APU con la cantidad

--- src/logic/ExcelBook.java
+++ src/logic/ExcelBook.java
@@ -19,71 +19,83 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import entities.TypeElement;
 
-
 public class ExcelBook {
 
 	private XSSFWorkbook workbook;
 	private ExcelSheet actualSheet;
 
-	public ExcelBook(){
+	public ExcelBook() {
 		workbook = new XSSFWorkbook();
 		actualSheet = new ExcelSheet();
 	}
 
 	public void selectSheet(String sheetName) {
 		try {
-			actualSheet.setSheet(workbook.getSheet(sheetName));			
+			actualSheet.setSheet(workbook.getSheet(sheetName));
 		} catch (Exception e) {
 			e.getMessage();
 			e.printStackTrace();
 
 		}
 	}
-	
-	public void putRowActualSheet(Integer key, Object[] objects ) {
+
+	public void putRowActualSheet(Integer key, Object[] objects) {
 		actualSheet.putRow(key, objects);
-	} 
-		
-	public void  newSheet(String sheetName) {
+	}
+	
+	public Object[] getRowActualSheet(Integer key) {
+		return this.actualSheet.getRow(key);
+	}
+	
+	public Object[] addToVector(Object object, Object[] originalVector) {
+		Object[] vectorAux = new Object[originalVector.length+1];
+		for (int i = 0; i < originalVector.length; i++) {
+			vectorAux[i] = originalVector[i];
+		}
+		vectorAux[originalVector.length]=object;
+		return vectorAux;
+	}
+
+	public void newSheet(String sheetName) {
 		actualSheet = new ExcelSheet();
 		actualSheet.setSheet(workbook.createSheet(sheetName));
 	}
-	
-	public void  writeActualSheet() {
+
+	public void writeActualSheet() {
 		actualSheet.writeSheet();
 	}
-	
-	public void  readSheet(String sheetName) {
+
+	public void readSheet(String sheetName) {
 		actualSheet.readSheet(sheetName);
 	}
-	
-	public String getCellStringValue(int key, int col){
+
+	public String getCellStringValue(int key, int col) {
 		return actualSheet.getCellStringValue(key, col);
-		
+
 	}
-	
+
 	public void createExcelFile(String path, String fileName) {
-		
+
 		try {
-			FileOutputStream out = new FileOutputStream(new File(path+fileName));
+			FileOutputStream out = new FileOutputStream(new File(path + fileName));
 			this.workbook.write(out);
 			out.close();
 		} catch (Exception e) {
 			e.getMessage();
 			e.printStackTrace();
-			
+
 		}
 	}
-	
+
 	public void readExcelFile(String path, String fileName) {
-		try (FileInputStream file = new FileInputStream(new File(path+fileName))) {
+		try (FileInputStream file = new FileInputStream(new File(path + fileName))) {
 			this.workbook = new XSSFWorkbook(file);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-	}	
-	
-	//adiciones
+	}
+
+	// adiciones
 	public XSSFWorkbook getWorkbook() {
 		return workbook;
 	}
@@ -91,25 +103,47 @@ public class ExcelBook {
 	public ExcelSheet getActualSheet() {
 		return actualSheet;
 	}
-	
-	public Map<Integer, Object[]>  getDataMap(){
+
+	public Map<Integer, Object[]> getDataMap() {
 		return this.actualSheet.getDataMap();
 	}
-	
+
 	public String getCellType(int key, int col) {
 //		this.actualSheet.getCellFormulaValue(key, col)
 		return this.actualSheet.getCellValue(key, col);
 //		return this.actualSheet.getCellValue(key, col);
 	}
-	
+
 	public Double getCellnumeric(int key, int col) {
 		return this.actualSheet.getCellNumericValue(key, col);
 	}
 	
 	public Map<String, APU> extractItems(String hoja){
 		Map<String, APU> map=new HashMap<>();
+		for (int i = 0; i < this.actualSheet.getDataMap().size(); i++) {
+			for (int j = 0; j < this.actualSheet.getDataMap().get(i).length; j++) {
+				if(getCellType(i, j).equals("MEMORIA DE CANTIDADES")) {
+					APU apu = new APU();
+					if(!hoja.contains("PRESUPUESTO")) {
+						System.out.println(hoja+" -----  "+this.actualSheet.getCellValue(12,0));
+						apu.setCodigo(this.actualSheet.getCellValue(12,0)+"-0");
+						try {
+							apu.setCantidad(this.actualSheet.getCellNumericValue(47, 38));							
+						} catch (Exception e) {
+							// TODO: handle exception
+						}
+						map.put(apu.getCodigo(), apu);
+					}
+				}
+			}
+		}
+		return map;
+	}
+
+	
+	public Map<String, APU> extractItemsAPU(String hoja){
+		Map<String, APU> map=new HashMap<>();
 		
-		ArrayList<String> items = new ArrayList<String>();
 		for (int i = 0; i < this.actualSheet.getDataMap().size(); i++) {
 			for (int j = 0; j < this.actualSheet.getDataMap().get(i).length; j++) {
 				if(getCellType(i, j).equals("ÍTEM")) {
@@ -123,6 +157,10 @@ public class ExcelBook {
 						apu.setValor(getCostoDirecto(i));
 						apu.setValor(apu.getValor().replaceAll("\\.", ","));
 						map.put(apu.getCodigo(), apu);
+					}else {
+//						System.out.println(this.actualSheet.getCellValue(16, 31));
+//						System.out.println(map.get(hoja+"-0"));
+//						map.get(hoja+"-0").setCantidad(this.actualSheet.getCellNumericValue(16, 31));
 					}
 				}
 			}
@@ -130,57 +168,92 @@ public class ExcelBook {
 		return map;
 	}
 	
-	public void extractCantidad(String hoja) {
+	public double extractCantidades(String hoja){
+		if(!hoja.contains("APU")) {
 		for (int i = 0; i < this.actualSheet.getDataMap().size(); i++) {
 			for (int j = 0; j < this.actualSheet.getDataMap().get(i).length; j++) {
 				if(getCellType(i, j).equals("ÍTEM")) {
-					//se va a extraer la cantidad
+						return this.actualSheet.getCellNumericValue(16, 31);
+					}
 				}
 			}
 		}
+		return 0.0;
 	}
 	
+//	public Map<String, APU> extractItems(String hoja, XSSFSheet sheet){
+//		Map<String, APU> map=new HashMap<>();
+//		
+//		ArrayList<String> items = new ArrayList<String>();
+//		for (int i = 0; i < this.actualSheet.getDataMap().size(); i++) {
+//			for (int j = 0; j < this.actualSheet.getDataMap().get(i).length; j++) {
+//				if(getCellType(i, j).equals("ÍTEM")) {
+//					APU apu = new APU();
+//					if(hoja.contains("APU")) {
+//						apu.setCodigo(this.actualSheet.getCellValue(i+1, j));
+//						apu.setCodigo(apu.getCodigo().replaceAll("\\.", "-"));
+//						apu.setNombre(this.actualSheet.getCellValue(i+1, j+2));
+//						apu.setNombre(apu.getNombre().replaceAll(";", ":"));
+//						apu.setUnidad(this.actualSheet.getCellValue(i+1, 'T'-'A'));
+//						apu.setValor(getCostoDirecto(i));
+//						apu.setValor(apu.getValor().replaceAll("\\.", ","));
+////						this.extractCantidad(sheet.getWorkbook().getSheet(apu.getCodigo().replaceAll("-0", "")));
+////						System.out.println(apu.getCodigo());
+//						map.put(apu.getCodigo(), apu);
+//					}
+//				}
+//			}
+//		}
+//		return map;
+//	}
+	
+//	public Double extractCantidad(XSSFSheet sheet) {
+//		ExcelSheet excelSheet = new ExcelSheet();
+//		excelSheet.setSheet(workbook.getSheet(sheet.getSheetName()));
+//		excelSheet.readSheet(sheet.getSheetName());
+//		for (int i = 0; i < excelSheet.getDataMap().size(); i++) {
+//			System.out.println("hace algo *********************************");
+//			for (int j = 0; j < excelSheet.getDataMap().get(i).length; j++) {
+//				System.out.println(sheet.getSheetName()+" -------- "+ excelSheet.getCellStringValue(0, 0));
+//			}
+//		}
+//		return 0.0;
+//	}
+
 	public String getCostoDirecto(int i) {
 		for (int ii = i; ii < this.actualSheet.getDataMap().size(); ii++) {
 			for (int jj = 0; jj < this.actualSheet.getDataMap().get(ii).length; jj++) {
-				if(getCellType(ii, jj).equals("TOTAL COSTO DIRECTO")) {
-					return this.actualSheet.getCellValue(ii, 'T'-'A');
-					
-//					for (int k = j+1; k < this.actualSheet.getDataMap().get(i).length; k++) {
-//						if (getCellType(i, k)!="" && this.actualSheet.getCellNumericValue(i, k)!=0) {
-//							apu.setValor(this.actualSheet.getCellNumericValue(i, k));
-//						}
-//					}
+				if (getCellType(ii, jj).equals("TOTAL COSTO DIRECTO")) {
+					return this.actualSheet.getCellValue(ii, 'T' - 'A');
 				}
 			}
 		}
 		return null;
-		
+
 	}
-	
-	
-	public ArrayList<String> extractNamesItems(){
+
+	public ArrayList<String> extractNamesItems() {
 		ArrayList<String> itemsNames = new ArrayList<String>();
 		for (int i = 0; i < this.actualSheet.getDataMap().size(); i++) {
 			for (int j = 0; j < this.actualSheet.getDataMap().get(i).length; j++) {
 //				System.out.print(getCellType(i,j)+" - ");
-				if(getCellType(i, j).equals("ÍTEM")) {
+				if (getCellType(i, j).equals("ÍTEM")) {
 //					System.out.println("*********  "+getCellType(i+1,j+1));
-					itemsNames.add(this.actualSheet.getCellFormulaValueString(i+1, j+2));
+					itemsNames.add(this.actualSheet.getCellFormulaValueString(i + 1, j + 2));
 				}
 			}
 //			System.out.println();
 		}
 		return itemsNames;
 	}
-	
-	public ArrayList<String> extractItemsTotalCosts(){
+
+	public ArrayList<String> extractItemsTotalCosts() {
 		ArrayList<String> itemsTotalCosts = new ArrayList<String>();
 		for (int i = 0; i < this.actualSheet.getDataMap().size(); i++) {
 			for (int j = 0; j < this.actualSheet.getDataMap().get(i).length; j++) {
-				if(getCellType(i, j).equals("TOTAL COSTO DIRECTO")) {
-					for (int k = j+1; k < this.actualSheet.getDataMap().get(i).length; k++) {
-						if (getCellType(i, k)!="" && this.actualSheet.getCellNumericValue(i, k)!=0) {
+				if (getCellType(i, j).equals("TOTAL COSTO DIRECTO")) {
+					for (int k = j + 1; k < this.actualSheet.getDataMap().get(i).length; k++) {
+						if (getCellType(i, k) != "" && this.actualSheet.getCellNumericValue(i, k) != 0) {
 							itemsTotalCosts.add(this.actualSheet.getCellValue(i, k));
 						}
 					}
@@ -189,20 +262,18 @@ public class ExcelBook {
 		}
 		return itemsTotalCosts;
 	}
-	
-	public Vector<String> extractTypesMaterials(){
+
+	public Vector<String> extractTypesMaterials() {
 		Vector<String> typesElements = new Vector<String>();
 		for (int i = 0; i < this.actualSheet.getDataMap().size(); i++) {
 			try {
-				if(getCellType(i, 0)!="" && ((getCellType(i, 0).charAt(0)=='I')
-								|| (getCellType(i, 0).charAt(0)=='V')
-								|| (getCellType(i, 0).charAt(0)=='X')
-								|| (getCellType(i, 0).charAt(0)=='L') ) && ((getCellType(i, 0).charAt(1)=='I')
-										|| (getCellType(i, 0).charAt(1)=='V')
-										|| (getCellType(i, 0).charAt(1)=='X')
-										|| (getCellType(i, 0).charAt(1)=='L')
-										||	(getCellType(i, 0).charAt(1)=='.')) ) {
-					if(!typesElements.contains(getCellType(i, 0))){
+				if (getCellType(i, 0) != ""
+						&& ((getCellType(i, 0).charAt(0) == 'I') || (getCellType(i, 0).charAt(0) == 'V')
+								|| (getCellType(i, 0).charAt(0) == 'X') || (getCellType(i, 0).charAt(0) == 'L'))
+						&& ((getCellType(i, 0).charAt(1) == 'I') || (getCellType(i, 0).charAt(1) == 'V')
+								|| (getCellType(i, 0).charAt(1) == 'X') || (getCellType(i, 0).charAt(1) == 'L')
+								|| (getCellType(i, 0).charAt(1) == '.'))) {
+					if (!typesElements.contains(getCellType(i, 0))) {
 						typesElements.add(getCellType(i, 0));
 					}
 				}
@@ -212,8 +283,8 @@ public class ExcelBook {
 		}
 		return typesElements;
 	}
-	
-	public Vector<TypeElement> createTypesMaterials(){
+
+	public Vector<TypeElement> createTypesMaterials() {
 		Vector<TypeElement> typesElements = new Vector<TypeElement>();
 		Vector<String> typesElementsAux = this.extractTypesMaterials();
 		for (int i = 0; i < typesElementsAux.size(); i++) {
@@ -223,49 +294,49 @@ public class ExcelBook {
 		}
 		return typesElements;
 	}
-    	
-	//-----------------------------
-	
-	//*********************************************************************************
-	
-	
 
+	// -----------------------------
 
+	// *********************************************************************************
 
 	public class ExcelSheet {
-		
-		private Map<Integer, Object[]> dataMap; 
+
+		private Map<Integer, Object[]> dataMap;
 		private XSSFSheet sheet;
-			
-		private ExcelSheet(){
+
+		private ExcelSheet() {
 			dataMap = new TreeMap<Integer, Object[]>();
-		}	
+		}
 
 		public void setSheet(XSSFSheet sheet) {
 			this.sheet = sheet;
 		}
-		
-		private void putRow(Integer key, Object[] objects ) {
-		   this.dataMap.put(key, objects);
-	    }
-		
-		private String getCellStringValue(int key, int col){
-			Object[] arregloObjetos = dataMap.get(key);
-			XSSFCell cell= (XSSFCell) arregloObjetos[col];
-			return cell.getStringCellValue();
-			
+
+		private void putRow(Integer key, Object[] objects) {
+			this.dataMap.put(key, objects);
 		}
 		
-		//adicion
-		private Double getCellNumericValue(int key, int col){
+		private Object[] getRow(Integer key) {
+			return this.dataMap.get(key);
+		}
+
+		private String getCellStringValue(int key, int col) {
 			Object[] arregloObjetos = dataMap.get(key);
-			XSSFCell cell= (XSSFCell) arregloObjetos[col];
+			XSSFCell cell = (XSSFCell) arregloObjetos[col];
+			return cell.getStringCellValue();
+
+		}
+
+		// adicion
+		private Double getCellNumericValue(int key, int col) {
+			Object[] arregloObjetos = dataMap.get(key);
+			XSSFCell cell = (XSSFCell) arregloObjetos[col];
 			return cell.getNumericCellValue();
 		}
-		
-		private String getCellFormulaValue(int key, int col){
+
+		private String getCellFormulaValue(int key, int col) {
 			Object[] arregloObjetos = dataMap.get(key);
-			XSSFCell cell= (XSSFCell) arregloObjetos[col];
+			XSSFCell cell = (XSSFCell) arregloObjetos[col];
 			switch (cell.getCachedFormulaResultType()) {
 			case Cell.CELL_TYPE_NUMERIC:
 				return String.valueOf(cell.getNumericCellValue());
@@ -276,70 +347,70 @@ public class ExcelBook {
 			}
 			return cell.getCellFormula();
 		}
-		
-		private String getCellFormulaValueString(int key, int col){
+
+		private String getCellFormulaValueString(int key, int col) {
 			Object[] arregloObjetos = dataMap.get(key);
-			XSSFCell cell= (XSSFCell) arregloObjetos[col];
+			XSSFCell cell = (XSSFCell) arregloObjetos[col];
 			return cell.getStringCellValue();
 		}
-		
+
 		public Map<Integer, Object[]> getDataMap() {
 			return dataMap;
 		}
-		
+
 		public String getCellValue(int key, int col) {
 			Object[] arregloObjetos = dataMap.get(key);
-			XSSFCell cell= (XSSFCell) arregloObjetos[col];
-			
+			XSSFCell cell = (XSSFCell) arregloObjetos[col];
+
 			if (cell == null) {
 				return "";
-			}else if (cell.getCellType()==Constants.CELL_TYPE_NUMERIC) {
-				return ""+getCellNumericValue(key,col);
-			}else if (cell.getCellType()==Constants.CELL_TYPE_STRING) {
-				return getCellStringValue(key,col);
-			}else if(cell.getCellType()==Constants.CELL_TYPE_FORMULA) {
-				return getCellFormulaValue(key,col);
+			} else if (cell.getCellType() == Constants.CELL_TYPE_NUMERIC) {
+				return "" + getCellNumericValue(key, col);
+			} else if (cell.getCellType() == Constants.CELL_TYPE_STRING) {
+				return getCellStringValue(key, col);
+			} else if (cell.getCellType() == Constants.CELL_TYPE_FORMULA) {
+				return getCellFormulaValue(key, col);
 //						+ " - "+getCellNumericValue(key,col);
-			}else
-			return " ";
+			} else
+				return " ";
 		}
-		
-		//**
-		
+
+		// **
+
 		private void writeSheet() {
-			
-	    	Iterator<Integer>  it = dataMap.keySet().iterator();
-	    	while(it.hasNext()){
-	    	  Integer key = it.next();
-	    	  XSSFRow row = sheet.createRow(key);
-	    	  Object[] arregloObjetos = dataMap.get(key);
-		      int numeroCelda = 0;
-		      for (Object obj : arregloObjetos) {
-		    	  XSSFCell cell = row.createCell(numeroCelda++);
-		    	 
-		    	  if (obj instanceof String) {
-		      		  cell.setCellValue((String) obj);
-		      		  
-		          } else if (obj instanceof Integer) {
-		        	  cell.setCellValue((Integer) obj);	        	  
-		          }
-		        }
-		    }
-	    }
-		
+
+			Iterator<Integer> it = dataMap.keySet().iterator();
+			while (it.hasNext()) {
+				Integer key = it.next();
+				XSSFRow row = sheet.createRow(key);
+				Object[] arregloObjetos = dataMap.get(key);
+				int numeroCelda = 0;
+				for (Object obj : arregloObjetos) {
+					XSSFCell cell = row.createCell(numeroCelda++);
+
+					if (obj instanceof String) {
+						cell.setCellValue((String) obj);
+
+					} else if (obj instanceof Integer) {
+						cell.setCellValue((Integer) obj);
+					}
+				}
+			}
+		}
+
 		private void readSheet(String sheetName) {
 			selectSheet(sheetName);
 			Iterator<Row> rowIterator = sheet.iterator();
- 			Row row;
- 			Integer rowNumber=0;
- 			while (rowIterator.hasNext()) {
+			Row row;
+			Integer rowNumber = 0;
+			while (rowIterator.hasNext()) {
 				row = rowIterator.next();
 				Iterator<Cell> cellIterator = row.cellIterator();
 				Cell cell = null;
 				Object[] objects = null;
 				try {
 //					objects = new Object[row.getLastCellNum()];		
-					objects = new Object[50];		
+					objects = new Object[50];
 				} catch (Exception e) {
 					// TODO: handle exception
 					System.out.println("no sirvio");
@@ -350,12 +421,8 @@ public class ExcelBook {
 					objects[col++] = cell;
 				}
 				putRowActualSheet(rowNumber++, objects);
-			}	
+			}
 		}
 
-		
-
-		
-		
 	}
 }
--- src/logic/LeerExcel.java
+++ src/logic/LeerExcel.java
@@ -13,63 +13,67 @@ import entities.TypeElement;
  * @author jeol
  */
 public class LeerExcel {
+	
+	private static int identificadorFila;
+	private static int identificadorFilaQuantity;
 
 	public static void main(String[] args) {
 		ExcelBook libro = new ExcelBook();
-    	
-    	//libro.readExcelFile("E:\\seta_projects\\testExcel\\", "PRESUPUESTO Y APU-ELECTRICOS-VR2.xlsx");
-		libro.readExcelFile("C://Users/LENOVO PC/Documents/", "RCI MEM Y APU (Autoguardado).xlsx");
+    	ExcelBook crearLibro = new ExcelBook();
+    	crearLibro.newSheet("RCI MEM Y APU (Autoguardado)");
+    	crearLibro.selectSheet("RCI MEM Y APU (Autoguardado)");
+		identificadorFila = 0;
+		identificadorFilaQuantity = 0;
+		crearLibro.putRowActualSheet(identificadorFila, new Object[]{"Item", "Descripcion", "Unidad","Vr. Unitario","Cantidad"});
+//    	libro.readExcelFile("E:\\seta_projects\\testExcel\\", "PRESUPUESTO Y APU-ELECTRICOS-VR2.xlsx");
+//		libro.readExcelFile("F:\\UPTC\\", "RED CONTRA INCENDIO PARA SISTEMAS.xlsx");
+		libro.readExcelFile("C:\\Users\\LENOVO PC\\Documents\\", "RCI MEM Y APU (Autoguardado).xlsx");
     	int num = libro.getWorkbook().getNumberOfSheets();
     	for (int i = 0; i < num; i++) {
     		XSSFSheet hoja = libro.getWorkbook().getSheetAt(i);
-//    		System.out.println(hoja.getSheetName());
-    		
-    		leerLibro(libro, hoja.getSheetName());	
-    		
-//    		else 
-//    			if(!hoja.getSheetName().contains("PRESUPUESTO"))
-//    			System.out.println(hoja.getSheetName());
+    		if (hoja.getSheetName().contains("APU")) {
+    			leerLibro(libro, crearLibro, hoja.getSheetName());	
+			}else
+				leerLibroNoNameApu(libro, crearLibro, hoja.getSheetName());
 		}    
-	}
-	
-    public static void leerLibro(ExcelBook libro, String hoja) {
-       
-    		
-//    	int colum = -1;
-//    	double acum = 0;
-//    	double acumTotal = 0;
-//    	boolean isSubtotal = false;
-    	libro.readSheet(hoja);
-    	
-//    	Vector<TypeElement> typeElements = libro.createTypesMaterials();
-    	
-    		Map<String, APU> items = libro.extractItems(hoja);
-//    	ArrayList<String> totalCosts = libro.extractItemsTotalCosts();
-//    	Map<String,String> cantidades;
-    	for(String codigo:items.keySet()) {
-    		System.out.println(codigo+";"+items.get(codigo).getNombre()+";"+items.get(codigo).getUnidad()+";"+items.get(codigo).getValor()+";"+items.get(codigo).getCantidad());
-    	}
-    	
-//    	ArrayList<Double> subTotals = new ArrayList<Double>();
-    	
-//    			if(libro.getCellType(i, j).equals("Vr Unitario")||libro.getCellType(i, j).equals("Vr. Unitario")) {
-//    				colum = j;
-//    			}
-//    			if(libro.getCellType(i, j).equals("SUBTOTAL")||libro.getCellType(i, j).equals("TOTAL COSTO DIRECTO")) {
-//    				isSubtotal = true;
-//    				if(acum!=0) {
-//    					subTotals.add(acum);
-//    				}
-//    				acum = 0;
-//    			}
-			
-//    		try {
-//    			if (!isSubtotal && libro.getCellnumeric(i, colum)!=0) {
-//    				acum += libro.getCellnumeric(i, colum);
-//				}
-//    			isSubtotal = false;
-//			} catch (Exception e) {
-//				// TODO: handle exception
+    	crearLibro.writeActualSheet();
+		crearLibro.createExcelFile("C:/Users/LENOVO PC/Documents/", "PresupuestoGeneral.xlsx");
+		
+//		crearLibro.putRowActualSheet(identificadorFila, new Object[]{"Item", "Descripcion", "Unidad","Vr. Unitario","Cantidad"});
+//		libro.readExcelFile("E:\\seta_projects\\testExcel\\", "PRESUPUESTO Y APU-ELECTRICOS-VR2.xlsx");
+//		crearLibro.newSheet("Electricos");
+//    	crearLibro.selectSheet("Electricos");
+//		identificadorFila = 0;
+//		crearLibro.putRowActualSheet(identificadorFila, new Object[]{"Item", "Descripcion", "Unidad","Vr. Unitario","Cantidad"});
+//		num = libro.getWorkbook().getNumberOfSheets();
+//    	for (int i = 0; i < num; i++) {
+//    		XSSFSheet hoja = libro.getWorkbook().getSheetAt(i);
+//    		if (hoja.getSheetName().contains("APU")) {
+//    			leerLibro(libro, crearLibro, hoja.getSheetName());	
 //			}
+//		}    
+//    	crearLibro.writeActualSheet();
+//		crearLibro.createExcelFile("C:/Users/LENOVO PC/Documents/", "PresupuestoGeneral.xlsx");
+		
+   	}
+	
+	public static void leerLibro(ExcelBook libro, ExcelBook crearLibro, String hoja) {
+    	libro.readSheet(hoja);
+   		Map<String, APU> apus = libro.extractItemsAPU(hoja);
+    	for(String codigo:apus.keySet()) {
+    		identificadorFila++;
+    		crearLibro.putRowActualSheet(identificadorFila, new Object[]{codigo, apus.get(codigo).getNombre(), apus.get(codigo).getUnidad(),apus.get(codigo).getValor()});
+    	}
+    }
+	
+    public static void leerLibroNoNameApu(ExcelBook libro, ExcelBook crearLibro, String hoja) {
+    	libro.readSheet(hoja);
+    	Map<String, APU> items = libro.extractItems(hoja);
+    	//Imprime solo cantidades
+    	for(String codigo:items.keySet()) {
+    		identificadorFilaQuantity++;
+    		Object[] objectVectorAux = crearLibro.addToVector(items.get(codigo).getCantidad()+"", crearLibro.getRowActualSheet(identificadorFilaQuantity));
+    			crearLibro.putRowActualSheet(identificadorFilaQuantity, new Object[] {objectVectorAux[0],objectVectorAux[1],objectVectorAux[2],objectVectorAux[3],objectVectorAux[4]});
+    	}
     }
 }
